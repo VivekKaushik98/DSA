f9b2a7b576dd1aab5fc54eed742c22b5e0ae1ac0
VivekKaushik98 <vivekkaushik1998@gmail.com>
2020-11-25 09:28:51 +0530

    Find Sum of Ascii for char index divisible by 3 start index 1

---
+++ AsciiIndex1.java
@@ -0,0 +1,43 @@
+import java .util.*;
+
+class AsciiIndex1{
+    public static void main(String args[]){
+
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+        char[] ch=new char[str.length()];
+        
+        for(int i=0;i<str.length();i++)
+        {
+            ch[i]=str.charAt(i);
+        }
+        int s=0;
+        int flag=0;
+        if(str.length()<3)
+        {
+            System.out.println(0);
+        }
+        
+        
+        else {
+            for(int i=0;i<str.length();i++)
+            {
+                if(( (int)ch[i]>=65 &&(int)ch[i]<=90 || (int)ch[i]>=97 &&(int)ch[i]<=127)){
+            if((i+1)%3==0 && i!=0 )
+                {
+                   //System.out.println(i+3);
+                    s=s+(int)ch[i];
+                }
+                }
+                else{
+                    flag=1;
+                    System.out.println(str+" is not a valid string"); 
+                    break;
+                }
+            }
+            if(flag==0)
+            System.out.println("Sum is "+s);
+        }
+
+    }
+}
