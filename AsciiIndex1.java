f4d67dcc1b78dcbdcc6b5c91d2135c0d9fec0689
VivekKaushik98 <vivekkaushik1998@gmail.com>
2020-11-25 15:40:20 +0530

    Optimised Ascii Sum divisible by 3 ignoring space ascii

--- AsciiIndex1.java
+++ AsciiIndex1.java
@@ -7,10 +7,11 @@ class AsciiIndex1{
         String str=sc.nextLine();
         char[] ch=new char[str.length()];
         
-        for(int i=0;i<str.length();i++)
-        {
-            ch[i]=str.charAt(i);
-        }
+        // for(int i=0;i<str.length();i++)
+        // {
+        //     ch[i]=str.charAt(i);
+        // }
+        ch = str.toCharArray();
         int s=0;
         int flag=0;
         if(str.length()<3)
@@ -25,10 +26,11 @@ class AsciiIndex1{
                 if(( (int)ch[i]>=65 &&(int)ch[i]<=90 || (int)ch[i]>=97 &&(int)ch[i]<=127)||(int)ch[i]==' '){
             if((i+1)%3==0 && i!=0 )
                 {
-                   //System.out.println(i+3);
+                  
                     s=s+(int)ch[i];
                     if(ch[i]==' ')
                     s=s-32;
+                    
                 }
                 }
                 else{
